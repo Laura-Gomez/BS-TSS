8040197227c018f1207f36ad4287a9aa4ae4d5bf
lgomez <lgomez@inmegen.gob.mx>
2019-03-14 18:01:18 -0600

    example code

---
+++ example_code.sh
@@ -0,0 +1,1 @@
+Rscript --vanilla Distance_TSS_BS.R BindingSiteSet.txt OmpR OmpR-RNASeq-PeakTargets.csv OmpR-All.csv pm_w_first_transc_g__w_ids_noHT.txt StorzG_TSS_Table_M63_0.4.txt Example-Data/INPUT
